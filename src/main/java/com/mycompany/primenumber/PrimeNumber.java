6e6c033bca50c6753d04f923a77625d1afaa59e8
Infolinia <piotr.rachwal002@gmail.com>
2022-05-03 21:17:12 +0200

    Poprawka warunku

--- src/main/java/com/mycompany/primenumber/PrimeNumber.java
+++ src/main/java/com/mycompany/primenumber/PrimeNumber.java
@@ -18,7 +18,7 @@ public class PrimeNumber {
        Integer[] numbers = new Integer[] {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
        
         for (Integer number : numbers) {
-            if(isPrime(number) == true){
+            if(isPrime(number)){
                 System.out.println(number + " to liczba pierwsza.");
             }else{
                 System.out.println(number + " to nie liczba pierwsza.");
